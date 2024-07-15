807928a28e8b8e985468f29ecad6adf11198485e
Marcelle Vargas <marcellecode@gmail.com>
2024-07-15 14:31:18 -0300

    [feat]- add animation on menu mobile

--- src/app/components/MobileMenu.tsx
+++ src/app/components/MobileMenu.tsx
@@ -8,9 +8,9 @@ const MobileMenu =() => {
     return (
         <div className="">
             <div className="flex flex-col gap-[4.5px] cursor-pointer"  onClick={()=> setIsOpen((prev)=> !prev)}>
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "rotate-45" : ""} origin-left`}></div>
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "rotate-45" : ""} origin-left ease-in-out duration-500`}></div>
                 <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "opacity-0" : ""}`}></div>
-                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "-rotate-45" : ""} origin-left`}></div>
+                <div className={`w-6 h-1 bg-blue-500 rounded-sm ${isOpen ? "-rotate-45" : ""} origin-left ease-in-out duration-500`}></div>
             </div>
             {isOpen && (
                 <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
